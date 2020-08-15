8ba58ceb02e7050aabe9145457b01c819234d4d3
Will Lytle <wlytle89@gmail.com>
2020-08-15 18:15:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   
-  
-  
-  # Print a numbered list of each element
+  array.each_with_index do |dwarf, index|
+    puts ""
+  end
 end
